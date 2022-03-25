da3679e90076631cefdddbef54f1f62c873b5b70
mangosteen <2982802228@qq.com>
2022-03-25 17:08:52 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	number = "0123456789"
+	letter = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+	symbol = ",.<>/?!@#$%^&*()_-+~;{}[]"
+)
+
+func main() {
+	route := gin.Default()
+	route.Static("/static", "./static")
+	route.LoadHTMLGlob("static/**/*")
+	route.GET("/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
+	route.GET("/create", func(ctx *gin.Context) {
+		length, _ := strconv.ParseInt(ctx.Query("length"), 10, 64)
+		isMix, _ := strconv.ParseBool(ctx.Query("isMix"))
+		rand.Seed(time.Now().Unix())
+		symbols := make([]string, 0)
+		symbols = append(symbols, strings.Split(number, "")...)
+		symbols = append(symbols, strings.Split(letter, "")...)
+		if isMix {
+			// 带特殊符号
+			symbols = append(symbols, strings.Split(symbol, "")...)
+		}
+		num := len(symbols)
+		var pwd = ""
+		for i := int64(0); i < length; i++ {
+			pwd = fmt.Sprint(pwd, symbols[rand.Intn(num)])
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"pwd": pwd,
+		})
+	})
+	route.Run(":52989")
+}
