9cde69ee9285d9acd44f72429636c3a6150075ef
mangosteen <2982802228@qq.com>
2022-03-25 17:45:47 +0800

    update

--- main.go
+++ main.go
@@ -24,26 +24,24 @@ func main() {
 		ctx.HTML(http.StatusOK, "index.html", nil)
 	})
 	route.GET("/create", func(ctx *gin.Context) {
-		go func(ctx *gin.Context) {
-			length, _ := strconv.ParseInt(ctx.Query("length"), 10, 64)
-			isMix, _ := strconv.ParseBool(ctx.Query("isMix"))
-			rand.Seed(time.Now().Unix())
-			symbols := make([]string, 0)
-			symbols = append(symbols, strings.Split(number, "")...)
-			symbols = append(symbols, strings.Split(letter, "")...)
-			if isMix {
-				// 带特殊符号
-				symbols = append(symbols, strings.Split(symbol, "")...)
-			}
-			num := len(symbols)
-			var pwd = ""
-			for i := int64(0); i < length; i++ {
-				pwd = fmt.Sprint(pwd, symbols[rand.Intn(num)])
-			}
-			ctx.JSON(http.StatusOK, gin.H{
-				"pwd": pwd,
-			})
-		}(ctx)
+		length, _ := strconv.ParseInt(ctx.Query("length"), 10, 64)
+		isMix, _ := strconv.ParseBool(ctx.Query("isMix"))
+		rand.Seed(time.Now().Unix())
+		symbols := make([]string, 0)
+		symbols = append(symbols, strings.Split(number, "")...)
+		symbols = append(symbols, strings.Split(letter, "")...)
+		if isMix {
+			// 带特殊符号
+			symbols = append(symbols, strings.Split(symbol, "")...)
+		}
+		num := len(symbols)
+		var pwd = ""
+		for i := int64(0); i < length; i++ {
+			pwd = fmt.Sprint(pwd, symbols[rand.Intn(num)])
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"pwd": pwd,
+		})
 	})
 	route.Run(":52989")
 }
